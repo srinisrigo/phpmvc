23693f48dc112c66eba6471ced5a410f257d0b94
srinisrigo <srinisrigo@gmail.com>
2018-04-13 12:57:33 +0530

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -1,13 +1,20 @@
 <?php
-  class PagesController {
-    public function home() {
-      $first_name = 'Jon';
-      $last_name  = 'Snow';
-      require_once('views/pages/home.php');
+  class PostsController {
+    public function index() {
+      // we store all the posts in a variable
+      $posts = Post::all();
+      require_once('views/posts/index.php');
     }
 
-    public function error() {
-      require_once('views/pages/error.php');
+    public function show() {
+      // we expect a url of form ?controller=posts&action=show&id=x
+      // without an id we just redirect to the error page as we need the post id to find it in the database
+      if (!isset($_GET['id']))
+        return call('pages', 'error');
+
+      // we use the given id to get the right post
+      $post = Post::find($_GET['id']);
+      require_once('views/posts/show.php');
     }
   }
 ?>
