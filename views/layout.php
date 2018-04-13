8433e2481c2ce5241bece70bc73cdd5ead8be015
srinivasan <srinivasan.r@emdsys.com>
2018-04-13 18:30:35 +0530

    tested with real data

--- views/layout.php
+++ views/layout.php
@@ -1,20 +1,19 @@
-<DOCTYPE html>
+<!doctype html>
 <html>
   <head>
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
     <div class='header-div'>
-        <header>
-          <a href='/php_mvc_blog'>Home</a>
-          <a href='?controller=posts&action=index'>Posts</a>
-        </header>
+        <a href='?controller=pages&action=home'>Home</a>
+        <a href='?controller=posts&action=index'>Posts</a>
     </div>
     <div class='content-div'>
+        <?php ini_set("memory_limit",-1); ?>
         <?php require_once('routes.php'); ?>
     </div>
     <div class='footer-div'>
       <?php echo date('l jS \of F Y'); ?>
     </div>
-  <body>
-<html>
+  </body>
+</html>
--- views/posts/index.php
+++ views/posts/index.php
@@ -1,8 +1,43 @@
-<p>Here is a list of all cpts:</p>
-
-<?php foreach($cpts as $cpt) { ?>
-  <p>
-    <?php echo $cpt->display_code; ?>
-    <a href='?controller=posts&action=show&id=<?php echo $cpt->id; ?>'>See content</a>
-  </p>
-<?php } ?>
+<table style="margin-left: auto;margin-right: auto;">
+  <caption>
+    Here is a list of all CPTs
+  </caption>
+  <tfoot>  
+      <td colspan=2>
+          <?php echo count($cpts); ?> records
+      </td>
+  </tfoot>
+  <?php if (count($cpts)) foreach($cpts as $cpt) { ?>
+  <tbody>
+    <tr>
+      <td>
+        <a href='?controller=posts&action=show&id=<?php echo $cpt->id; ?>'>View</a>
+      </td>
+      <td>
+        <?php echo $cpt->ref_code; ?>
+      </td>
+      <td>
+        <?php echo $cpt->display_code; ?>
+      </td>
+      <td>
+        <?php echo $cpt->short_description; ?>
+      </td>
+      <td>
+        <?php echo $cpt->display_description; ?>
+      </td>
+      <td>
+        <?php echo $cpt->duration; ?>
+      </td>
+      <td>
+        <?php echo $cpt->units; ?>
+      </td>
+      <td>
+        <?php echo $cpt->body_part; ?>
+      </td>
+      <td>
+        <?php echo $cpt->notes; ?>
+      </td>
+    </tr>
+  </tbody>
+  <?php } ?>
+</table>
